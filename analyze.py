54efb40e22e9921a07551e56d2f0e9863eaf3043
varshithams <65653455+varshithams@users.noreply.github.com>
2024-08-30 19:00:24 -0400

    uploading 3 files for lab1

---
+++ analyze.py
@@ -0,0 +1,55 @@
+from azure.cognitiveservices.vision.computervision import ComputerVisionClient
+from msrest.authentication import CognitiveServicesCredentials
+from azure.cognitiveservices.vision.computervision.models import OperationStatusCodes
+import time
+import os
+
+from dotenv import load_dotenv
+
+#loading key and endpoint from .env file
+load_dotenv()
+
+endpoint = os.getenv('AZURE_ENDPOINT')
+key = os.getenv('AZURE_KEY')
+
+credentials = CognitiveServicesCredentials(key)
+
+client = ComputerVisionClient(
+    endpoint=endpoint,
+    credentials=credentials
+)
+
+def read_image(uri):
+    numberOfCharsInOperationId = 36
+    maxRetries = 10
+
+    # SDK call
+    rawHttpResponse = client.read(uri, language="en", raw=True)
+
+    # Get ID from returned headers
+    operationLocation = rawHttpResponse.headers["Operation-Location"]
+    idLocation = len(operationLocation) - numberOfCharsInOperationId
+    operationId = operationLocation[idLocation:]
+
+    # SDK call
+    result = client.get_read_result(operationId)
+    
+    # Try API
+    retry = 0
+    
+    while retry < maxRetries:
+        if result.status.lower () not in ['notstarted', 'running']:
+            break
+        time.sleep(1)
+        result = client.get_read_result(operationId)
+        
+        retry += 1
+    
+    if retry == maxRetries:
+        return "max retries reached"
+
+    if result.status == OperationStatusCodes.succeeded:
+        res_text = " ".join([line.text for line in result.analyze_result.read_results[0].lines])
+        return res_text
+    else:
+        return "error"
